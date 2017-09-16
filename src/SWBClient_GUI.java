403854ab32c69531cda3caa71ef8b4d923e1d619
XmTX1994 <31956052+XmTX1994@users.noreply.github.com>
2017-09-16 19:24:56 +1000

    modify

--- src/SWBClient_GUI.java
+++ src/SWBClient_GUI.java
@@ -14,46 +14,232 @@ public class SWBClient_GUI {
     private JButton bt_save;
     private JButton bt_open;
     private JButton bt_saveas;
-    private JButton bt_pen;
-    private JComboBox cb_degree;
+    private JButton bt_pencil;
+    private JComboBox cb_pencil;
     private JButton bt_color;
     private JButton bt_shape;
     private JPanel board;
+    private JTextField text_info;
+    private JButton button1;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JPanel cl000;
+    private JPanel cl111;
+    private JPanel cl666;
+    private JPanel clccc;
+    private JPanel cl100;
+    private JPanel cl150;
+    private JPanel cl110;
+    private JPanel cl510;
+    private JPanel cl010;
+    private JPanel cl015;
+    private JPanel cl011;
+    private JPanel cl051;
+    private JPanel cl001;
+    private JPanel cl501;
+    private JPanel cl101;
+    private JPanel cl105;
+    private JButton bt_eraser;
+    private JComboBox cb_eraser;
     private int startx;
     private int starty;
     private int x;
     private int y;
-    private String state = "pen";
-//        private JColorChooser chooser;
+    Graphics g;
+    private Color color = Color.black;
+    private Color eraser_color = Color.white;
+    private String state = "pencil";
 
     public SWBClient_GUI() {
+//        set color
+        cl000.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(0,0,0);
+            }
+        });
+        cl111.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(255,255,255);
+            }
+        });
+        cl666.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(102,102,102);
+            }
+        });
+        clccc.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(204,204,204);
+            }
+        });
+        cl100.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(255,0,0);
+            }
+        });
+        cl150.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(255,128,0);
+            }
+        });
+        cl110.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(255,255,0);
+            }
+        });
+        cl510.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(128,255,0);
+            }
+        });
+        cl010.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(0,255,0);
+            }
+        });
+        cl015.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(0,255,128);
+            }
+        });
+        cl011.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(0,255,255);
+            }
+        });
+        cl051.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(0,128,255);
+            }
+        });
+        cl001.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(0,0,255);
+            }
+        });
+        cl501.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(128,0,255);
+            }
+        });
+        cl101.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(255,0,255);
+            }
+        });
+        cl105.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                color = new Color(255,0,128);
+            }
+        });
+
+
+
+//click color bt
         bt_color.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                Color color = new Color(255,255,255);
-                JColorChooser.showDialog(null,"choose color" ,color);
+//                Color color = new Color(255,255,255);
+                color = JColorChooser.showDialog(null,"choose color" ,color);
             }
         });
-//        mainWindow.addMouseMotionListener(new MouseMotionAdapter() {
-//            @Override
-//            public void mouseMoved(MouseEvent e) {
-//                super.mouseMoved(e);
-//
-//
-//                Graphics g;
-//                g=mainWindow.getGraphics();
-//                g.setColor(Color.BLACK);
-//                x = e.getX();
-//                y = e.getY();
-//                g.drawLine(startx,starty,x,y);
-//                System.out.println(x+","+y);
-//                startx = e.getX();
-//                starty = e.getY();
-//
-//
-//            }
-//        });
+//        update position information
+        board.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                x = e.getX();
+                y = e.getY();
+                text_info.setText("Position: ("+x+","+y+")");
+                System.out.println(x+","+y);
+            }
+        });
+//        pen drawing
+        board.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                startx = e.getX();
+                starty = e.getY();
+            }
+        });
+        board.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                super.mouseDragged(e);
+                if (state == "pencil"){
+                    g = board.getGraphics();
+                    g.setColor(color);
+                    x = e.getX();
+                    y = e.getY();
+                    g.drawLine(startx,starty,x,y);
+                    text_info.setText("Position: ("+x+","+y+")");
+                    System.out.println(x+","+y);
+                    startx = e.getX();
+                    starty = e.getY();
+                }else if(state == "eraser"){
+                    g = board.getGraphics();
+                    g.setColor(eraser_color);
+                    x = e.getX();
+                    y = e.getY();
+                    g.drawLine(startx,starty,x,y);
+                    text_info.setText("Position: ("+x+","+y+")");
+                    System.out.println(x+","+y);
+                    startx = e.getX();
+                    starty = e.getY();
+                }
+            }
+        });
+
+        bt_pencil.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                state = "pencil";
+                System.out.println("state pencil");
+            }
+        });
+        bt_eraser.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                state = "eraser";
+                System.out.println("state eraser");
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -65,4 +251,7 @@ public class SWBClient_GUI {
         frame.setVisible(true);
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
