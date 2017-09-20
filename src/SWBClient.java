a12b889982ca69a6c3db8b824c3fb739f8ac9a36
xiaonainiu <es930119@gmail.com>
2017-09-20 23:26:39 +1000

    By es
    Update server and client

--- src/SWBClient.java
+++ src/SWBClient.java
@@ -1,5 +1,50 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 /**
  * Created by ES on 2017/9/14.
  */
 public class SWBClient {
+//    InputStream is = new InputStream();
+    private Socket socket;
+    private DataOutputStream dos;
+    private DataInputStream dis;
+
+    public static void main(String args[]){
+        /**
+         * by ES
+         * Client
+         * start
+         */
+//        initialize GUI
+        SWBClient client = new SWBClient();
+//        SWBClient_GUI gui = new SWBClient_GUI();
+
+        try{
+            while (true){
+//                socket = this.socket;
+                client.socket = new Socket("127.0.0.1", 1234);
+                client.dis = new DataInputStream(client.socket.getInputStream());
+                client.dos = new DataOutputStream(client.socket.getOutputStream());
+                String str = null;
+                if ((str = client.dis.readUTF()) != null){
+                    //get the command and make operation
+                    System.out.println();
+                }
+                client.dis.close();
+                client.socket.close();
+            }
+        }catch (UnknownHostException e){
+            e.printStackTrace();
+
+        }catch (SocketException e){
+            e.printStackTrace();
+
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+    }
 }
--- src/SWBServer.java
+++ src/SWBServer.java
@@ -1,5 +1,23 @@
+import org.json.*;
+import java.util.*;
 /**
  * Created by ES on 2017/9/14.
  */
 public class SWBServer {
+    public static void main(String[] args){
+        System.out.println("**Server Start**");
+
+        try{
+            
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        HashMap<String,String> map = new HashMap<String,String>();
+        map.put("operation","chawWindow");
+        JSONObject message = new JSONObject(map);
+        String a = message.toString();
+        JSONObject received_Message = new JSONObject(a);
+        System.out.print(received_Message.get("operation"));
+    }
 }
